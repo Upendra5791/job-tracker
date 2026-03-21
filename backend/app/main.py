35f4b704fdd0e86991f6af6cf863d19f39d5ffd1
upenz5791 <upenz5791@gmail.com>
2026-03-21 23:46:39 +0000

    added jobs router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.concurrency import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import auth
+from app.routers import auth, jobs
 from app.database import create_db_tables
 
 
@@ -11,12 +11,13 @@ async def lifespan(app: FastAPI):
     create_db_tables()
     yield
     print("Shutting down...")
-    
+
+
 app = FastAPI(title="Job Tracker API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # update to allow frontend localhost only
+    allow_origins=["*"],  # update to allow frontend localhost only
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -27,7 +28,8 @@ app.add_middleware(
 def read_root():
     return {"message": "Welcome to the Job Tracker API!"}
 
+
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
-# app.include_router(jobs.router, prefix="/api/jobs", tags=["jobs"])
+app.include_router(jobs.router, prefix="/api/jobs", tags=["jobs"])
 # app.include_router(ai.router, prefix="/api/ai", tags=["ai"])
 # app.include_router(stats.router, prefix="/api/stats", tags=["stats"])
